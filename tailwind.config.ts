829e71ebad91b44dbf12dc10344c918b445cbb02
Akram-00 <aliakram9789@gmail.com>
2024-11-13 17:19:14 +0530

    font is setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,23 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        regular: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
+      fontStyle: {
+        italic: 'italic',
+      },
     },
   },
   plugins: [],
