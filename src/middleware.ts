3335785a31d7e77009b1b2e6b30a62e5d81edbb8
Akram-00 <aliakram9789@gmail.com>
2024-12-17 23:47:25 +0530

    unUsed cookie import removed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { handleFirebaseResponse } from './services/firebase/auth';
 import { fetchRoleByFirebaseId } from './services/firebase/fetchUserById';
-import Cookie from 'cookie'
 
 
 
@@ -24,11 +23,11 @@ export async function middleware(req: NextRequest) {
   if (url.pathname.startsWith('/dashboard')) {
 
     const cookieHeader = req.headers.get('cookie') || '';
-    const cookies = Object.fromEntries(
-      cookieHeader
-        .split(';')
-        .map(cookie => cookie.trim().split('=').map(decodeURIComponent))
-    );
+  const cookies = Object.fromEntries(
+    cookieHeader
+      .split(';')
+      .map(cookie => cookie.trim().split('=').map(decodeURIComponent))
+  );
 
     const token = cookies.authToken;
     const uid = cookies.userUID;
